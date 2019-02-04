03913c1612b78dfc444b855dc73b65915770b8a7
iamRaziAkram <37903391+iamRaziAkram@users.noreply.github.com>
2019-02-04 21:58:15 +0530

    Update UserAdminControllerIntegrationTest.java

--- movieapp-api/src/test/java/upgrad/movieapp/api/controller/UserAdminControllerIntegrationTest.java
+++ movieapp-api/src/test/java/upgrad/movieapp/api/controller/UserAdminControllerIntegrationTest.java
@@ -16,35 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import upgrad.movieapp.api.model.UserDetailsResponse;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserAdminControllerIntegrationTest {
 
-    @LocalServerPort
-    private int port;
-
-    private URL base;
-
-    @Autowired
-    private TestRestTemplate template;
-
-    @Before
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/api/v1/users");
-    }
-
-    
-    public void getUser() throws Exception {
-        final ResponseEntity<UserDetailsResponse> response = template.getForEntity(base.toString() + "/7d174a25-ba31-45a8-85b4-b06ffc9d5f8f",UserDetailsResponse.class);
-        assertEquals(HttpStatus.OK,response.getStatusCode());
-        final UserDetailsResponse responseBody = response.getBody();
-        assertEquals(responseBody.getId(), expected().getId());
-    }
-
-    private UserDetailsResponse expected() {
-        UserDetailsResponse response = new UserDetailsResponse();
-        response.setId("7d174a25-ba31-45a8-85b4-b06ffc9d5f8f");
-        return response;
-    }
 
 }
